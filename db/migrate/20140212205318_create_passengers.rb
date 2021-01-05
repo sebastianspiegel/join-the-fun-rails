3d9412041af3ec18a550ec4ab8f818c521521c05
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-05 10:59:04 -0500

    commit tree 0deb14490e8d8184ea9d92e84ab540f7ae36478f

--- db/migrate/20140212205318_create_passengers.rb
+++ db/migrate/20140212205318_create_passengers.rb
@@ -2,7 +2,6 @@ class CreatePassengers < ActiveRecord::Migration
   def change
     create_table :passengers do |t|
       t.timestamps null: false
-      t.integer :taxi_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "taxi_id"
   end
 
   create_table "rides", force: :cascade do |t|
@@ -26,9 +25,8 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   end
 
   create_table "taxis", force: :cascade do |t|
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "passenger_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
